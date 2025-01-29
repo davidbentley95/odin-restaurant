cf4b2616d50c420954ed895e8fc545836780b95a
David Bentley <david.bentley95@gmail.com>
2025-01-29 06:56:43 -0800

    Set up basic CSS and homepage module
    Create homepage module to load homepage when nav
    button is clicked.
    
    Create CSS to style the page.

---
+++ src/home.js
@@ -0,0 +1,24 @@
+import img1 from "./img1.jpg";
+
+const loadHomePage = function() {
+    const contentDiv = document.querySelector("#content");
+
+    const img = document.createElement("img");
+    const textContainer = document.createElement("div");
+    const welcomeText = document.createElement("h1");
+    const homeParagraph = document.createElement("p");
+
+    textContainer.classList.add("text-container");
+
+    img.src = img1;
+
+    welcomeText.innerHTML = "Welcome to Savory Bliss – Where Every Bite Feels Like Home";
+
+    homeParagraph.innerHTML = "Discover the joy of delicious, freshly-prepared meals crafted with love. Whether you're here for a casual lunch or a cozy dinner, we promise a memorable dining experience.";
+
+    textContainer.append(welcomeText, homeParagraph);
+
+    contentDiv.append(img, textContainer);
+};
+
+export { loadHomePage };
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+import { loadHomePage } from "./home.js";
+
+document.querySelector("#home").addEventListener("click", loadHomePage);
